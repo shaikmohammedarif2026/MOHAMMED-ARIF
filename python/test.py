24b4905b0900a099623547ed42a40cf15e2f896d
S.Mohammed Arif <shaikmoammedarif1999@gamil.com>
2026-02-13 00:29:17 +0530

    init commit

--- python/test.py
+++ python/test.py
@@ -1,8 +1,7 @@
-n = 7
-fact = 1
-i = 1
-while i <= n:
-    fact *= i
-    i += 1
-print("factorial=",fact)
-
+t = [[1,2],[3,4],[5,6]]
+v=[]
+for i in t:
+    for j in i:
+        b =list(map(lambda j: j+5, i ))
+        v.append(b)
+        print(v)
